abcf63c434931fd29a6cca630f5ea277cf0ed039
devansh2001 <dponda3@gatech.edu>
2020-08-07 12:44:39 -0400

    extracted text and variables to compile

--- QuestionsFactory.py
+++ QuestionsFactory.py
@@ -19,13 +19,30 @@ class QuestionsFactory:
         return ''
 
     def load_questions(self):
+        data = None
         with open(r'./questions.yaml') as file:
             file = yaml.safe_load(file)
-            return json.dumps(file, indent=2)
+            data_str = json.dumps(file, indent=2)
+        data_dict = json.loads(data_str)
+        questions = data_dict['questions']
+
+        out = []
+        for question in questions:
+            text = question['text']
+            if 'variables' in question:
+                variables = question['variables']
+                print(variables)
+                text = self.produce_cleaned_question(text, variables)
+
+
+
+        return json.dumps(data_dict)
+        
 
     def generate_questions(self):
-        app = self.choose_application()
-        permission = self.choose_permission(app)
-        benign = self.choose_benign_purpose(permission)
-        malicious = self.choose_malicious_purpose(permission)
-        return []
+        # app = self.choose_application()
+        # permission = self.choose_permission(app)
+        # benign = self.choose_benign_purpose(permission)
+        # malicious = self.choose_malicious_purpose(permission)
+        # return []
+        return self.load_questions()
