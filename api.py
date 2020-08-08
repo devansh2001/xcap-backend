859e54e1f4c9196e4b7c47b95fd8d176fc96807d
devansh2001 <dponda3@gatech.edu>
2020-08-07 21:16:26 -0400

    troubleshooted cors error

--- api.py
+++ api.py
@@ -1,13 +1,18 @@
 from flask import Flask, request
+from flask_cors import CORS
 import QuestionsFactory
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 @app.route('/')
 def test():
+    print('hello')
     return 'Hello, World!'
 
-@app.route('/get-questions')
+@app.route('/get-questions', methods=['POST'])
 def get_questions():
+    print("hey")
     apps_and_permissions = request.data
     factory = QuestionsFactory.QuestionsFactory(None)
     questions = factory.generate_questions()
