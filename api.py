2f047ed431166c06e8f0ae3c747df43d7f85aa69
devansh2001 <dponda3@gatech.edu>
2020-08-10 23:01:57 -0400

    accepting form submission from web-app using POST API endpoint

--- api.py
+++ api.py
@@ -24,5 +24,19 @@ def get_questions():
     return questions
 
 
+@app.route('/submit-survey', methods=['POST'])
+def submit_survey():
+    print('Hit Submit Survey Endpoint')
+    data = None
+    print(request.data)
+    if request.data:
+        data = json.loads(request.data)
+    
+    print('Received data:')
+    print(data)
+
+    return {}
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
