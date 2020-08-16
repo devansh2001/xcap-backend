c8e72639aee17b50e08a674bd48e29c53261e067
devansh2001 <dponda3@gatech.edu>
2020-08-16 17:22:26 -0400

    config

---
+++ config.py
@@ -0,0 +1,11 @@
+firebaseConfig = {
+    "apiKey": "AIzaSyCpPJxUn2Q1NthIzXic30MA4lUaVp8-qw8",
+    "authDomain": "xcap-data-store.firebaseapp.com",
+    "databaseURL": "https://xcap-data-store.firebaseio.com",
+    "projectId": "xcap-data-store",
+    "storageBucket": "xcap-data-store.appspot.com",
+    "serviceAccount": "./xcap-data-store-firebase-adminsdk-zj35y-859abd18c7.json",
+    "messagingSenderId": "145131999889",
+    "appId": "1:145131999889:web:b29423da6d621a82796530",
+    "measurementId": "G-7V96MKLN3Z"
+}
