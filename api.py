2e310b9f07576f96ee6cb0b2f6e2326efb412b76
devansh2001 <dponda3@gatech.edu>
2020-09-27 17:40:33 -0400

    changed to env vars

--- api.py
+++ api.py
@@ -4,11 +4,34 @@ import json
 import pyrebase
 import QuestionsFactory
 import datetime
-from config import firebaseConfig
+import os
+# from config import firebaseConfig
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
-
+# print(firebaseConfig)
+firebaseConfig = {
+    "apiKey": os.environ['apiKey'],
+    "authDomain": os.environ['authDomain'],
+    "databaseURL": os.environ['databaseURL'],
+    "projectId": os.environ['projectId'],
+    "storageBucket": os.environ['storageBucket'],
+    "serviceAccount": {
+        "type": os.environ['type'],
+        "project_id": os.environ['project_id'],
+        "private_key_id": os.environ['private_key_id'],
+        "private_key": os.environ['private_key'],
+        "client_email": os.environ['client_email'],
+        "client_id": os.environ['client_id'],
+        "auth_uri": os.environ['auth_uri'],
+        "token_uri": os.environ['token_uri'],
+        "auth_provider_x509_cert_url": os.environ['auth_provider_x509_cert_url'],
+        "client_x509_cert_url": os.environ['client_x509_cert_url']
+    },
+    "messagingSenderId": os.environ['messagingSenderId'],
+    "appId": os.environ['appId'],
+    "measurementId": os.environ['measurementId']
+}
 firebase = pyrebase.initialize_app(firebaseConfig)
 database = firebase.database()
 
